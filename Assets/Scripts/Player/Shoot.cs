bc99c1e7dbb352e35fac2bc38353bbef26e8ca07
haenghaeng <haeng@pusan.ac.kr>
2024-12-12 14:46:58 +0900

    feat : Shoot
    오브젝트를 발사하는 대신 Ray를 발사하고 부딪힌 지점에 BulletMark 오브젝트를 생성합니다.
    현재는 Ray가 모든 오브젝트를 감지합니다. 일부 오브젝트는 Ray가 무시하는 기능을 추가해야 합니다.
    또한, 특정 상황에선 부딪히더라도 총알자국이 남지 않아야 합니다.

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 플레이어가 바라보는 방향으로 총을 발사합니다.
+/// </summary>
 public class Shoot : MonoBehaviour
 {
+    [Header("Components")]
     [SerializeField] private Transform playerCamera;
-    [SerializeField] private float speed = 10;
+    [SerializeField] private GameObject bulletMarkPrefab;
 
+    [Header("Variables")]
+    [SerializeField] private float speed = 10;
     [SerializeField] private float cycle = 0.2f; // 연사속도 = 발사 후 다시 발사할 때까지 걸리는 시간(단위 : 초)
     private WaitForSeconds cycleWFS;
     private bool isKeyDown = false;
@@ -18,6 +24,8 @@ public class Shoot : MonoBehaviour
 
     private void Update()
     {
+        // 마우스 왼쪽키를 누르면 총을 발사하기 시작합니다.
+        // 키를 떼면 발사를 멈춥니다.
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             isKeyDown = true;
@@ -30,32 +38,31 @@ public class Shoot : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// cycleWFS 주기마다 플레이어의 중심에서 플레이어가가 바라보는 방향으로 Ray를 발사합니다.</br>
+    /// 가장 먼저 부딪힌 지점에 bulletMark를 남깁니다.
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator Shooting()
     {
         while(isKeyDown)
         {
-            GameObject bullet = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            bullet.GetComponent<Collider>().enabled = false;
-            bullet.transform.position = transform.position;
-            bullet.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Ray ray = new Ray();
+            ray.origin = transform.position;
+            ray.direction = playerCamera.forward;
 
-            Rigidbody rb = bullet.AddComponent<Rigidbody>();
-            rb.useGravity = false;
+            RaycastHit raycastHit;
+            bool isHit = Physics.Raycast(ray, out raycastHit);
+            
 
-            rb.velocity = playerCamera.forward * speed;
-
-            StartCoroutine(asdf(bullet));
+            if (isHit)
+            {
+                GameObject bulletMark = Instantiate(bulletMarkPrefab, raycastHit.point, bulletMarkPrefab.transform.rotation);
+            }
 
             yield return cycleWFS;
         }
 
-        yield return null;
-    }
-
-    private IEnumerator asdf(GameObject obj)
-    {
-        yield return new WaitForSeconds(3);
-        Destroy(obj);
         yield return null;
     }
 
